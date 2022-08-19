0fc1521d409a4a79d7fdcd227c430600aab8ec94
mircea1711 <maiereanmircea@cnprbeclean.ro>
2022-08-19 11:35:30 +0300

    Translated to Romanian Language

--- js/script.js
+++ js/script.js
@@ -1,62 +1,61 @@
-const form = document.getElementById('generate-form');
-const qr = document.getElementById('qr-code');
+const form = document.getElementById("generate-form");
+const qr = document.getElementById("qr-code");
 
 const onGenerateSubmit = (e) => {
   clearUI();
   e.preventDefault();
 
-  const url = document.getElementById('url').value;
-  const size = document.getElementById('size').value;
+  const url = document.getElementById("url").value;
+  const size = document.getElementById("size").value;
 
   if (url === "") {
     alert("Please enter a URL");
-  }
-  else {
+  } else {
     showSpinner();
     setTimeout(() => {
       hideSpinner();
-      generateQRCode(url, size)
+      generateQRCode(url, size);
       setTimeout(() => {
-        const saveUrl = qr.querySelector('img').src;
+        const saveUrl = qr.querySelector("img").src;
         createSaveBtn(saveUrl);
-      }, 50)
-    }, 1000)
+      }, 50);
+    }, 1000);
   }
-}
+};
 
 const generateQRCode = (url, size) => {
-  const qrcode = new QRCode('qr-code', {
+  const qrcode = new QRCode("qr-code", {
     text: url,
     width: size,
     height: size,
   });
-}
+};
 
 const showSpinner = () => {
   document.getElementById("spinner").style.display = "block";
-}
+};
 
 const hideSpinner = () => {
   document.getElementById("spinner").style.display = "none";
-}
+};
 
 const clearUI = () => {
-  qr.innerHTML = ''
+  qr.innerHTML = "";
   const saveLink = document.getElementById("save-link");
-  if (saveLink)
-    saveLink.remove();
-}
+  if (saveLink) saveLink.remove();
+};
 
 const createSaveBtn = (saveUrl) => {
-  const link = document.createElement('a');
-  link.id = 'save-link'
-  link.classList = 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5'
-  link.href = saveUrl
-  link.download = 'qr-code'
-  link.innerHTML = 'Save Image'
-  document.getElementById('generated').appendChild(link);
-}
+  const link = document.createElement("a");
+  link.id = "save-link";
+  link.classList =
+    "bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-3/4 md:w-1/3 m-auto my-5";
+  link.href = saveUrl;
+  link.download = "qr-code";
+  link.innerHTML = "Salvează imaginea";
+  document.getElementById("generated").appendChild(link);
+};
 
 hideSpinner();
 
-form.addEventListener('submit', onGenerateSubmit)
+form.addEventListener("submit", onGenerateSubmit);
